8fb2315255d06cf5970f9df52e6b33a392308b3a
vignesh14052002 <vignesharivazhagan1409@gmail.com>
2022-07-13 18:27:44 +0530

    -

--- empty-example/node.js
+++
@@ -1,8 +0,0 @@
-let handleRequest = function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-};
-
-const http = require('http');
-let server = http.createServer(handleRequest);
-server.listen(8080);
